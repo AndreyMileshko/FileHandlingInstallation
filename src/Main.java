b6b21e63b0cb2c3648df2fe12dffdf5fb9b40075
Andrey Mileshko <AndreyPrograming.Mileshko@yandex.ru>
2024-07-01 15:39:34 +0700

    Corrected and deleted the .idea folder

--- src/Main.java
+++ src/Main.java
@@ -3,51 +3,65 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         File dirGames = new File("C:\\Users\\andre\\IdeaProjects\\FileHandlingInstallation\\games");
         StringBuilder sb = new StringBuilder();
 
         //В папке Games создайте несколько директорий: src, res, savegames, temp.
-        File dirSrc = new File(dirGames, "src");
-        if (dirSrc.mkdir()) appendedLogs(sb, dirSrc);
-        File dirRes = new File(dirGames, "res");
-        if (dirRes.mkdir()) appendedLogs(sb, dirRes);
-        File dirSavegames = new File(dirGames, "savegames");
-        if (dirSavegames.mkdir()) appendedLogs(sb, dirSavegames);
-        File dirTemp = new File(dirGames, "temp");
-        if (dirTemp.mkdir()) appendedLogs(sb, dirTemp);
+        List<String> listDirGames = new ArrayList<>(Arrays.asList("src", "res", "savegames", "temp"));
+        for (String name : listDirGames) {
+            createDirectoryAddLogs(dirGames.getPath(),name,sb);
+        }
 
         //В каталоге src создайте две директории: main, test.
-        File dirMain = new File(dirSrc, "main");
-        if (dirMain.mkdir()) appendedLogs(sb, dirMain);
-        File dirTest = new File(dirSrc, "test");
-        if (dirTest.mkdir()) appendedLogs(sb, dirTest);
+        List<String> listDirSrc = new ArrayList<>(Arrays.asList("main", "test"));
+        for (String name : listDirSrc) {
+            createDirectoryAddLogs(dirGames.getPath() + "\\src",name,sb);
+        }
 
         //В подкаталоге main создайте два файла: Main.java, Utils.java.
-        File fileMain = new File(dirMain, "Main.java");
-        if (fileMain.createNewFile()) appendedLogs(sb, fileMain);
-        File fileUtils = new File(dirMain, "Utils.java");
-        if (fileUtils.createNewFile()) appendedLogs(sb, fileUtils);
+        List<String> listFileMain = new ArrayList<>(Arrays.asList("Main.java", "Utils.java"));
+        for (String name : listFileMain) {
+            createFileAddLogs(dirGames.getPath() + "\\src\\main",name,sb);
+        }
 
         //В каталог res создайте три директории: drawables, vectors, icons.
-        File dirDrawables = new File(dirRes, "drawables");
-        if (dirDrawables.mkdir()) appendedLogs(sb, dirDrawables);
-        File dirVectors = new File(dirRes, "vectors");
-        if (dirVectors.mkdir()) appendedLogs(sb, dirVectors);
-        File dirIcons = new File(dirRes, "icons");
-        if (dirIcons.mkdir()) appendedLogs(sb, dirIcons);
+        List<String> listDirRes = new ArrayList<>(Arrays.asList("drawables", "vectors", "icons"));
+        for (String name : listDirRes) {
+            createDirectoryAddLogs(dirGames.getPath() + "\\res",name,sb);
+        }
 
         //В директории temp создайте файл temp.txt.
-        File fileTemp = new File(dirTemp, "temp.txt");
-        if (fileTemp.createNewFile()) appendedLogs(sb, fileTemp);
+        createFileAddLogs(dirGames.getPath() + "\\temp", "temp.txt", sb);
 
-        try(BufferedWriter bw = new BufferedWriter(new FileWriter(fileTemp))) {
-            bw.write(sb.toString());
+        writeLogs(dirGames.getPath() + "\\temp\\temp.txt", sb.toString());
+    }
+
+    public static void createFileAddLogs(String path, String name, StringBuilder sb) {
+        File file = new File(path, name);
+        try {
+            if (file.createNewFile()) {
+                appendedLogs(sb, file);
+                System.out.printf("Файл %s создан\n", file.getName());
+            }
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            System.out.printf("Файл %s не создан\n", file.getName());
+        }
+    }
+
+    public static void createDirectoryAddLogs(String path, String name, StringBuilder sb) {
+        File file = new File(path, name);
+        if (file.mkdir()) {
+            appendedLogs(sb, file);
+            System.out.printf("Директория %s создана\n", file.getName());
+        } else {
+            System.out.printf("Директория %s не создана\n", file.getName());
         }
     }
 
@@ -67,5 +81,14 @@ public class Main {
                 .append("время последнего изменения файла: ")
                 .append(time)
                 .append(" |\n");
+    }
+
+    public static void writeLogs(String temp, String logs) {
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(temp))) {
+            bw.write(logs);
+            System.out.println("Логи успешно записаны в файл.");
+        } catch (IOException e) {
+            System.out.println("При записи логов произошло исключение.");
+        }
     }
 }
